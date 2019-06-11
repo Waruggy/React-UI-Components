631e243a616d761d5ea97c8fe0dce16e00c5f39f
Charles <cwang8413@yahoo.com>
2019-06-10 22:18:54 -0700

    here is project-I

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Card.css';
+import CardContent from './cardContent';
+
+function CardBanner() {
+    return ( <
+        a className = "card-banner"
+        href = "https://www.reactjs.org" >
+        <
+        img className = "react-image"
+        src = "https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+        alt = "react logo" >
+        <
+        /img> <
+        CardContent / >
+        <
+        /a>
+    );
+}
+
+export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+
+function CardContainer(pops) {
+    return ( <
+        div className = "card" >
+        <
+        CardBanner / >
+        <
+        /div>
+    );
+}
+
+export default CardContainer;
