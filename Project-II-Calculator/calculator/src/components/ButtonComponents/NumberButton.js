82cf274d752ea39edfe079fcccbb72f2ccb104d0
Charles <cwang8413@yahoo.com>
2019-06-11 19:35:57 -0700

    Here is project II.

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props) {
+    return ( <
+        div className = {
+            props.class
+        } > {
+            props.text
+        } < /div>
+    );
+}
+
+export default NumberButton;
